c22c3ff179974439ab6bcf95c23b55858924e27f
liuhailong <hailong.liu@diehl.com>
2019-09-16 09:18:52 +0800

    init comment

---
+++ src/main/java/com/ruoyi/project/his/HisManageController.java
@@ -0,0 +1,113 @@
+package com.ruoyi.project.his;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 模态窗口
+ * 
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping(HisManageController.prefix)
+public class HisManageController
+{
+    public static final  String prefix = "/his";
+    public static final String INDEX = "/index";
+    public static final String SHENHE = "/shenhe";
+    public static final String DENGJIBU = "/dengjibu";
+    public static final String REPORT = "/report";
+
+    public static final String CRBGL = "/crbgl";
+    public static final String SWBLGL = "/swblgl";
+    public static final String SYXBLGL = "/syxblgl";
+    public static final String MXBJCGL = "/mxbjcgl";
+    public static final String LGBLJCGL = "/lgbljcgl";
+    public static final String YYHTZDGL = "/yyhtzdgl";
+    public static final String GWZSGL = "/gwzsgl";
+    public static final String NYZDGL = "/nyzdgl";
+
+
+    /**
+     * 模态窗口
+     */
+    @GetMapping("/{model}/{function}")
+    public String getPage(@PathVariable String model, @PathVariable String function)
+    {
+        return prefix + "/" + model + "/" + function;
+    }
+
+    /**
+     * 模态窗口
+     *//*
+    @GetMapping(CRBGL + INDEX)
+    public String index1()
+    {
+        return prefix + CRBGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(SWBLGL + INDEX)
+    public String index2()
+    {
+        return prefix + SWBLGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(SYXBLGL + INDEX)
+    public String index3()
+    {
+        return prefix + SYXBLGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(MXBJCGL + INDEX)
+    public String index4()
+    {
+        return prefix + MXBJCGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(NYZDGL + INDEX)
+    public String index5()
+    {
+        return prefix + NYZDGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(LGBLJCGL + INDEX)
+    public String index6()
+    {
+        return prefix + LGBLJCGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(YYHTZDGL + INDEX)
+    public String index7()
+    {
+        return prefix + YYHTZDGL + INDEX;
+    }
+
+    *//**
+     * 模态窗口
+     *//*
+    @GetMapping(GWZSGL + INDEX)
+    public String index8()
+    {
+        return prefix + GWZSGL + INDEX;
+    }*/
+}
---
+++ src/main/resources/static/his/static/js/js.js
@@ -0,0 +1,42 @@
+﻿
+function pickedFunccsrq() {
+
+    $dp.$('txt_csrq_ye').value = $dp.cal.getP('y');
+    $dp.$('txt_csrq_mo').value = $dp.cal.getP('M');
+    $dp.$('txt_csrq_da').value = $dp.cal.getP('d');
+    returnbirth();
+//    $dp.$('tb_jssj_s').value = $dp.cal.getP('H');
+//    $dp.$('tb_jssj_f').value = $dp.cal.getP('m');
+//    //$dp.$('d523_ss').value = $dp.cal.getP('s');
+}
+
+
+function pickedFuncfbsj() {
+
+    $dp.$('txt_fbrq_ye').value = $dp.cal.getP('y');
+    $dp.$('txt_fbrq_mo').value = $dp.cal.getP('M');
+    $dp.$('txt_fbrq_da').value = $dp.cal.getP('d');
+//    $dp.$('tb_fbsj_s').value = $dp.cal.getP('H');
+//    $dp.$('tb_fbsj_f').value = $dp.cal.getP('m');
+    //$dp.$('d523_ss').value = $dp.cal.getP('s');
+}
+
+function pickedFunczdrq() {
+
+    $dp.$('txt_zdrq_ye').value = $dp.cal.getP('y');
+    $dp.$('txt_zdrq_mo').value = $dp.cal.getP('M');
+    $dp.$('txt_zdrq_da').value = $dp.cal.getP('d');
+    $dp.$('txt_zdrq_hh').value = $dp.cal.getP('H');
+    $dp.$('txt_zdrq_mi').value = $dp.cal.getP('m');
+    //$dp.$('d523_ss').value = $dp.cal.getP('s');
+}
+
+function pickedFuncswsj() {
+
+    $dp.$('txt_swrq_ye').value = $dp.cal.getP('y');
+    $dp.$('txt_swrq_mo').value = $dp.cal.getP('M');
+    $dp.$('txt_swrq_da').value = $dp.cal.getP('d');
+//    $dp.$('tb_swsj_s').value = $dp.cal.getP('H');
+//    $dp.$('tb_swsj_f').value = $dp.cal.getP('m');
+    //$dp.$('d523_ss').value = $dp.cal.getP('s');
+}
